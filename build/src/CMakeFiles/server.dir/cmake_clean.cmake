21cdf6ba96d4c7e3d462f5045f3127011db2d4ff
ljy <ljy.sj@qq.com>
2023-05-26 15:00:52 +0800

    test

---
+++ build/src/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../../lib/libserver.a"
+  "../../lib/libserver.pdb"
+  "CMakeFiles/server.dir/Acceptor.cpp.o"
+  "CMakeFiles/server.dir/Buffer.cpp.o"
+  "CMakeFiles/server.dir/Channel.cpp.o"
+  "CMakeFiles/server.dir/Connection.cpp.o"
+  "CMakeFiles/server.dir/Epoll.cpp.o"
+  "CMakeFiles/server.dir/EventLoop.cpp.o"
+  "CMakeFiles/server.dir/InetAddress.cpp.o"
+  "CMakeFiles/server.dir/Server.cpp.o"
+  "CMakeFiles/server.dir/Socket.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
